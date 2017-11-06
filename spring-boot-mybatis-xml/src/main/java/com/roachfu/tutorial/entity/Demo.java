7933ca5a5c31053aaf6e9ab9b20a77e59fb207cc
roachfu <fuqiang.it@gmail.com>
2017-11-06 22:34:06 +0800

    refactor(#mybatis-xml): 重构mybatis-xml项目，新增demo属性，不全测试流程

--- spring-boot-mybatis-xml/src/main/java/com/roachfu/tutorial/entity/Demo.java
+++ spring-boot-mybatis-xml/src/main/java/com/roachfu/tutorial/entity/Demo.java
@@ -1,19 +1,28 @@
 package com.roachfu.tutorial.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author roach
  * @datetime 2017/11/1 15:16
  */
 public class Demo implements Serializable{
-    
+
+    private static final long serialVersionUID = -6605419946999027600L;
+
     private Integer id;
     
     private String demoName;
     
     private String demoValue;
-    
+
+    private Integer demoStatus;
+
+    private Date createTime;
+
+    private Date updateTime;
+
     public Integer getId() {
         return id;
     }
@@ -36,5 +45,29 @@ public class Demo implements Serializable{
     
     public void setDemoValue(String demoValue) {
         this.demoValue = demoValue;
+    }
+
+    public Integer getDemoStatus() {
+        return demoStatus;
+    }
+
+    public void setDemoStatus(Integer demoStatus) {
+        this.demoStatus = demoStatus;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
     }
 }
--- spring-boot-mybatis-xml/src/test/java/com/roachfu/tutorial/mapper/DemoMapperTest.java
+++ spring-boot-mybatis-xml/src/test/java/com/roachfu/tutorial/mapper/DemoMapperTest.java
@@ -2,12 +2,15 @@ package com.roachfu.tutorial.mapper;
 
 import com.roachfu.tutorial.entity.Demo;
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -17,14 +20,51 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DemoMapperTest {
-    
+
     @Resource
     private DemoMapper demoMapper;
-    
+
     @Test
-    public void testSelectList(){
+    public void testInsert() {
+        Demo demo = new Demo();
+        demo.setDemoName("roach name");
+        demo.setDemoValue("roach value");
+        demo.setId(1);
+        int flag = demoMapper.insert(demo);
+        Assert.assertEquals(flag, 1);
+    }
+
+    @Test
+    public void testSelectList() {
         List<Demo> demoList = demoMapper.selectList();
         Assert.assertEquals(1, demoList.size());
+    }
+
+    @Test
+    public void testSelectOne() {
+        Demo demo = demoMapper.selectOne(1);
+        Assert.assertEquals("roach name", demo.getDemoName());
+        Assert.assertEquals("roach value", demo.getDemoValue());
+    }
+
+    @Test
+    public void testUpdate() {
+        Demo demo = new Demo();
+        demo.setDemoName("crow name");
+        demo.setDemoValue("crow value ");
+        demo.setId(1);
+        demo.setUpdateTime(new Date());
+        int flag = demoMapper.update(demo);
+        Assert.assertEquals(1, flag);
+    }
+
+    @Test
+    public void testDelete() {
+        int flag = demoMapper.delete(1);
+        Assert.assertEquals(flag, 1);
+        Demo demo = demoMapper.selectOne(1);
+        Assert.assertEquals(demo, null);
     }
 }
