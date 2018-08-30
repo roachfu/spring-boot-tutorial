06bdefc3d403860afd764d518f9430375e09cd2f
fuqiang <fuqiang@imagedt.com>
2018-08-30 11:49:24 +0800

    feature: 新增配置中心学习

---
+++ spring-cloud-config-git/spring-cloud-config-server/src/main/java/com/roachfu/tutorial/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.roachfu.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author roach
+ * @date 2018/8/30
+ */
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
